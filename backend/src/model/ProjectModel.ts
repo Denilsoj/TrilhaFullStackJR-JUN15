9bdd827eb28d6962e01727129a123a40ab5a4970
Denilson Oliveira <denilso-oliveira@pop-os.localdomain>
2024-07-23 08:18:57 -0300

    feat/add-model-for-project

---
+++ backend/src/model/ProjectModel.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+
+const ProjectSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  srcPicture: {
+    type: String,
+    require: true,
+  },
+  hightlights: {
+    type: Boolean,
+    require: true,
+    default: true,
+  },
+});
+
+const ProjectModel = mongoose.model('project', ProjectSchema);
+
+export default ProjectModel;
